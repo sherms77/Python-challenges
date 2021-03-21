b33c6ee2650528da2304d6e06d0b8fe035691b7c
sherms77 <sherms77@gmail.com>
2021-03-22 10:06:37 +1100

    modified:   HackerRank/Python if-Else/ifElse_hr_myCode(3).py 	modified:   HackerRank/Python if-Else/notes.txt 	new file:   HackerRank/Python if-Else/screenshots/both function calls_wrong answer.png 	new file:   HackerRank/Python if-Else/screenshots/both tests wrong_if-Else.png 	new file:   HackerRank/Python if-Else/screenshots/custom in[24]_comp success.png 	new file:   HackerRank/Python if-Else/screenshots/custom in[3]_comp success.png 	new file:   HackerRank/Python if-Else/screenshots/custom in[both]_comp success.png 	new file:   HackerRank/Python if-Else/screenshots/no function calls_wrong answer.png 	new file:   HackerRank/Python if-Else/screenshots/one call in[3]_wrong answer.png 	new file:   HackerRank/Python if-Else/screenshots/one call[24]_wrong answer.png 	new file:   HackerRank/Python if-Else/screenshots/test 01 wrong_if-Else.png

--- HackerRank/Python if-Else/ifElse_hr_myCode(3).py
+++ HackerRank/Python if-Else/ifElse_hr_myCode(3).py
@@ -17,12 +17,12 @@ def main_combined(a):
         print('Not Weird')
     
     # Rule 3: if n is even and in the inclusive range of 6 to 20 print Weird
-    elif a in range(6,20) and even == 0: # (y%2) == 0: - replaced with even variable
+    elif a in range(6,20) and even == 0:
         print('Weird')
     
     # Rule 4: if n is even and greater than 20 print Not Weird
     elif a > 20 and (a%2) == 0:
         print('Not Weird')
     
-main_combined(3) # works - out[3]: Weird
+# main_combined(3) # works - out[3]: Weird
 main_combined(24) # works - out[24]: Not weird
