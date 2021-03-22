29a787a522273beb8668793eb32dc28f5f2c71b4
sherms77 <sherms77@gmail.com>
2021-03-23 07:13:21 +1100

    new file:   HackerRank/Python if-Else/ifElse_hr_myCode(4).py 	modified:   HackerRank/Python if-Else/notes.txt 	renamed:    HackerRank/Python if-Else/screenshots/one call in[3]_wrong answer.png -> HackerRank/Python if-Else/screenshots/one call in[24]_wrong answer.png 	renamed:    HackerRank/Python if-Else/screenshots/one call[24]_wrong answer.png -> HackerRank/Python if-Else/screenshots/one call[3]_wrong answer.png 	modified:   README.md

---
+++ HackerRank/Python if-Else/ifElse_hr_myCode(4).py
@@ -0,0 +1,34 @@
+# step 6 | method 2
+import math, os, random, re, sys
+
+def main_combined(a):
+    even = a%2
+
+    # constraint: 1 <= n <= 100 | no output if input invalid
+    if a <= 1 or a >= 100:
+        print('Invalid input')
+    
+    # Rule 1: if n is odd print Weird
+    elif even != 0:
+        print('Weird')
+
+    # Rule 2: if n is even and in the inlcusive range of 2 to 5 print Not Weird
+    elif a in range(2,5) and even == 0: 
+        print('Not Weird')
+    
+    # Rule 3: if n is even and in the inclusive range of 6 to 20 print Weird
+    elif a in range(6,20) and even == 0:
+        print('Weird')
+    
+    # 220321
+    # Rule 4: if n is even and greater than 20 print Not Weird
+    # changed to else
+    else: 
+        print('hello') # this works
+        '''
+        a > 20 and (a%2) == 0:
+            print('Not Weird')
+        '''
+
+# main_combined(3) # works - out[3]: Weird
+main_combined(24) # changed last elif to else, didn't work. Should be - out[24]: Not weird
--- README.md
+++ README.md
@@ -2,8 +2,12 @@
 Python coding challenges.
 
 # Sources
-How to think like a computer scientist: Learning with Python - Apr 2002: First edition. |edabit.com. | 
-HackerRank.
-
+How to think like a computer scientist: Learning with Python - Apr 2002: First edition. | edabit.com. | HackerRank.
+1. How to think like a computer scientist: Learning with Python - Apr 2002: First edition.
+2. edabit.com. 
+3. HackerRank.
+ 
 # Status
-Active. Working on: Python if-Else (HackerRank). Not started: abecedarian (Think cs).
+- Active. 
+- Working on: Python if-Else (HackerRank). 
+- Not started: abecedarian (Think cs).
