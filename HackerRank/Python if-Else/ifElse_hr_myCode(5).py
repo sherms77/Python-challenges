8cd57abb3b01f8e25ae5d65411f256169a66e03f
sherms77 <sherms77@gmail.com>
2021-04-05 09:21:49 +1000

    renamed:    HackerRank/Python if-Else/archive/ifElse_hR_myCode.py -> HackerRank/Python if-Else/archive/ifElse_hR_myCode(1).py 	renamed:    HackerRank/Python if-Else/ifElse_hr_myCode(4).py -> HackerRank/Python if-Else/ifElse_hr_myCode(5).py 	new file:   HackerRank/Python if-Else/ifElse_hr_myCode(6).py 	modified:   HackerRank/Python if-Else/notes.txt

--- HackerRank/Python if-Else/ifElse_hr_myCode(5).py
+++ HackerRank/Python if-Else/ifElse_hr_myCode(5).py
@@ -9,18 +9,20 @@ Rule 4: if n is even and greater than 20 print Not Weird
 
 # 020421: submitted this code in hr, it passed the test cases but failed 1/8 test cases when submitted.
 # 020421: refer to screenshot failed sub_010421.png.
+# 030421: modified code in rule 1 from elif n != n%2 to elif n % 2 != 0.
+# 030421: it passed test cases but failed 3/8 test cases when I submitted it.
 
 import math, os, random, re, sys
 
 if __name__ == '__main__':
-    n = int(input().strip())
+    n = int(input('Enter number:').strip())
     
     # Rule 4: if n is even and greater than 20 print Not Weird
     if n > 20 and n%2 == 0:
         print('Not Weird')
 
     # Rule 1: if n is odd print Weird
-    elif n != n%2:
+    elif n % 2 != 0:
         print('Weird')
     
     # Rule 2: if n is even and in the inlcusive range of 2 to 5 print Not Weird
---
+++ HackerRank/Python if-Else/ifElse_hr_myCode(6).py
@@ -0,0 +1,56 @@
+# wrapped in function to test multliple test cases
+
+import math, os, random, re, sys
+
+def evenOdd (n):
+   
+    # Rule 4: if n is even and greater than 20 print Not Weird
+    if n > 20 and n%2 == 0:
+        print('Not Weird')
+
+    # Rule 1: if n is odd print Weird
+    elif n%2 != 0:
+        print('Weird')
+    
+    # Rule 2: if n is even and in the inlcusive range of 2 to 5 print Not Weird
+    elif n%2 in range (2,6):
+        print('Not Weird')
+
+    # Rule 3: if n is even and in the inclusive range of 6 to 20 print Weird
+    elif n%2 in range (6,20): # elif n%2 in range (7,20):
+        print('Not Weird')
+'''
+# rule 1
+evenOdd(3) # out[3]: Weird - Correct
+evenOdd(1) # out[1]: Weird - Correct
+'''
+
+# rule 2
+# GOT ONE OUTPUT OF WEIRD. INCORRECT. NEED TO DEBUG TO SEE WHAT IS GOING ON.
+evenOdd(2) # out[2]: Not Weird
+evenOdd(4) # out[4]: Not Weird
+evenOdd(5) # out[5]: Not Weird
+
+'''
+# rule 3
+# out[rule 3]: Not Weird
+evenOdd(6) # 6 is not included in rules 2 or 3 but should be included in 3. modified to include 6.
+evenOdd(7)
+evenOdd(8)
+evenOdd(9)
+evenOdd(10)
+evenOdd(11)
+evenOdd(12)
+evenOdd(13)
+evenOdd(14)
+evenOdd(15)
+evenOdd(16)
+evenOdd(17)
+evenOdd(18)
+evenOdd(19)
+evenOdd(20)
+
+# rule 4
+evenOdd(24) # out[24]: Not Weird
+evenOdd(100) # out[100]: Not Weird
+'''
