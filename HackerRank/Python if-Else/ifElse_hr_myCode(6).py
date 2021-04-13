9f353b7e84d0848452778bfd2ec9c6ffda42384d
sherms77 <sherms77@gmail.com>
2021-04-14 09:12:24 +1000

    modified:   HackerRank/Python if-Else/ifElse_hr_myCode(6).py 	modified:   HackerRank/Python if-Else/notes.txt

--- HackerRank/Python if-Else/ifElse_hr_myCode(6).py
+++ HackerRank/Python if-Else/ifElse_hr_myCode(6).py
@@ -20,13 +20,13 @@ def evenOdd (n):
     elif n%2 == 0 and n in range (6,20): # elif n%2 in range (7,20):
         print('Not Weird')
 
-'''
+
 # rule 1
 evenOdd(3) # out[3]: Weird - Correct
 evenOdd(1) # out[1]: Weird - Correct
 evenOdd(5) # out[5]: Weird  - Correct
 
-090421: NEED TO TEST THIS BLOCK FOR RULE 1
+# 090421: NEED TO TEST THIS BLOCK FOR RULE 1
 evenOdd(7) # out[7]: Weird
 evenOdd(9) # out[9]: Weird
 evenOdd(11) # out[11]: Weird
@@ -34,15 +34,12 @@ evenOdd(13) # out[13]: Weird
 evenOdd(15) # out[15]: Weird
 evenOdd(17) # out[17]: Weird
 evenOdd(19) # out[19]: Weird
-'''
+# 130421: Tested outputs for this block - ALL CORRECT
 
-'''
 # rule 2
 evenOdd(2) # out[2]: Not Weird - Correct
 evenOdd(4) # out[4]: Not Weird - Correct
-'''
 
-'''
 # rule 3
 # the number 6 is not included in rules 2 or 3 but should be included in 3. modified to include 6.
 evenOdd(6) # out[6]: Not Weird 
@@ -53,12 +50,9 @@ evenOdd(14) # out[14]: Not Weird
 evenOdd(16) # out[16]: Not Weird
 evenOdd(18) # out[18]: Not Weird
 evenOdd(20) # out[20]: Not Weird
-
 # 090421: Tested outputs for rule 3 - ALL CORRECT
-'''
 
 # rule 4
 evenOdd(24) # out[24]: Not Weird
 evenOdd(100) # out[100]: Not Weird
-
 # 120421: Tested outputs for rule 4 - ALL CORRECT
