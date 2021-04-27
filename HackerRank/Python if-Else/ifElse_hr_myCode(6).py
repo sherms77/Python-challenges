2c88b872081f979fdf2ca878348e00e5ec5d9e5a
sherms77 <sherms77@gmail.com>
2021-04-28 06:45:12 +1000

    modified:   HackerRank/Python if-Else/ifElse_hr_myCode(6).py 	modified:   HackerRank/Python if-Else/notes.txt

--- HackerRank/Python if-Else/ifElse_hr_myCode(6).py
+++ HackerRank/Python if-Else/ifElse_hr_myCode(6).py
@@ -3,31 +3,19 @@
 import math, os, random, re, sys
 
 def evenOdd (n):
-
     # constraint = 1 <= n <= 100
-    if n >=1 and n <= 100:
-   
-        # Rule 1: if n is odd print Weird
-        if n%2 != 0:
-            print('Weird')
-        
-        # Rule 2: if n is even and in the inlcusive range of 2 to 5 print Not Weird
-        elif n%2 == 0 and n in range (2,6):
-            print('Not Weird')
-
-        # Rule 3: if n is even and in the inclusive range of 6 to 20 print Weird
-        elif n%2 == 0 and n in range (6,21): # elif n%2 in range (7,20):
-            print('Not Weird')
-
-        # Rule 4: if n is even and greater than 20 print Not Weird
-        elif n > 20 and n%2 == 0:
-            print('Not Weird')
-
+    
+    if n % 2 != 0: # Rule 1: if n is odd print Weird
+        print('Weird')
     else:
-        print('Invalid')
-
-# constraint
-evenOdd(500) # out[500]: Invalid - Correct
+        if n >= 1 and n <= 5: # Rule 2: if n is even and in the inlcusive range of 2 to 5 print Not Weird
+            print('Not Weird')
+        elif n <= 20: # Rule 3: if n is even and in the inclusive range of 6 to 20 print Weird
+            print('Weird') 
+        else:
+            print('Not Weird') # Rule 4: if n is even and greater than 20 print Not Weird
+    
+# FUNCTION CALLS - TESTS
 
 '''
 # rule 1
@@ -35,7 +23,6 @@ evenOdd(3) # out[3]: Weird - Correct
 evenOdd(1) # out[1]: Weird - Correct
 evenOdd(5) # out[5]: Weird  - Correct
 
-# 090421: NEED TO TEST THIS BLOCK FOR RULE 1
 evenOdd(7) # out[7]: Weird
 evenOdd(9) # out[9]: Weird
 evenOdd(11) # out[11]: Weird
@@ -43,14 +30,12 @@ evenOdd(13) # out[13]: Weird
 evenOdd(15) # out[15]: Weird
 evenOdd(17) # out[17]: Weird
 evenOdd(19) # out[19]: Weird
-# 130421: Tested outputs for this block - ALL CORRECT
 
 # rule 2
 evenOdd(2) # out[2]: Not Weird - Correct
 evenOdd(4) # out[4]: Not Weird - Correct
 
 # rule 3
-# the number 6 is not included in rules 2 or 3 but should be included in 3. modified to include 6.
 evenOdd(6) # out[6]: Not Weird 
 evenOdd(8) # out[8]: Not Weird
 evenOdd(10) # out[10]: Not Weird
@@ -59,13 +44,13 @@ evenOdd(14) # out[14]: Not Weird
 evenOdd(16) # out[16]: Not Weird
 evenOdd(18) # out[18]: Not Weird
 evenOdd(20) # out[20]: Not Weird
-# 090421: Tested outputs for rule 3 - ALL CORRECT
-'''
 
-'''
 # rule 4
 evenOdd(24) # out[24]: Not Weird
 evenOdd(100) # out[100]: Not Weird
-# 120421: Tested outputs for rule 4 - ALL CORRECT
-# 250421: Moved rule 4 to top of function - works correctly
 '''
+
+# constraint
+evenOdd(500) # out[500]: 
+evenOdd(225) # out[225]: 
+# 270421: INCORRECT OUTPUT - NOT WEIRD AND WEIRD
