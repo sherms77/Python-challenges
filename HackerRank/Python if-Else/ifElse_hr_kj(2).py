680839b534f7178844cadeb41e72c0ebaadf16da
sherms77 <sherms77@gmail.com>
2021-04-30 06:56:30 +1000

    new file:   HackerRank/Python if-Else/analysis.txt 	modified:   HackerRank/Python if-Else/ifElse_hr_kj(2).py 	modified:   HackerRank/Python if-Else/notes.txt

--- HackerRank/Python if-Else/ifElse_hr_kj(2).py
+++ HackerRank/Python if-Else/ifElse_hr_kj(2).py
@@ -3,7 +3,7 @@ import math, os, random, re, sys
 if __name__ == '__main__':
     n = int(input().strip())
     
-    if (n > 1 and n < 100):
+    if (n > 1 and n < 100): 
         if (n%2 == 0):
             if (n > 20):
                 print("Not Weird")
